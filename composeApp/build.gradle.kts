1431758ab4428b2fa05435e362304043d7b02e89
tezov <tezov.app@gmail.com>
2026-01-03 23:29:14 +0100

    first commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,97 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+
+//            implementation("io.insert-koin:koin-android")
+//            implementation("io.insert-koin:koin-androidx-compose")
+//            implementation("io.insert-koin:koin-androidx-compose-navigation")
+//            implementation("io.insert-koin:koin-core-coroutines")
+//            implementation("io.insert-koin:koin-androidx-workmanager")
+//            implementation("io.insert-koin:koin-androidx-navigation:1.2.3-alpha03")
+//            testImplementation("io.insert-koin:koin-test-junit4")
+//            testImplementation("io.insert-koin:koin-android-test")
+
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+
+
+//            implementation("io.insert-koin:koin-bom")
+//            implementation("io.insert-koin:koin-core:4.2.0-beta2")
+            implementation("io.insert-koin:koin-compose:4.2.0-beta2")
+//            implementation("io.insert-koin:koin-compose-viewmodel")
+//            implementation("io.insert-koin:koin-compose-navigation3")
+
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.tezov.koineducation"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.tezov.koineducation"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
+
---
+++ composeApp/src/commonMain/kotlin/com/tezov/koineducation/App.kt
@@ -0,0 +1,64 @@
+package com.tezov.koineducation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import koineducation.composeapp.generated.resources.Res
+import koineducation.composeapp.generated.resources.compose_multiplatform
+import org.koin.compose.KoinApplication
+import org.koin.compose.KoinMultiplatformApplication
+import org.koin.core.context.startKoin
+import org.koin.dsl.koinConfiguration
+
+@Composable
+@Preview
+fun App() {
+    KoinApplication(
+        configuration = koinConfiguration{
+
+        },
+        content = { PrivateApp() }
+    )
+}
+
+@Composable
+@Preview
+private fun PrivateApp() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
+        }
+    }
+}
